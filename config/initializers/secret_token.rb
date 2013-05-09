0f3e47f4706c81977a2bbd46c5e7dbaf4613699f
Marc Karzen <mkarzen@relishmix.com>
2013-05-08 22:40:21 -0700

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xdc::Application.config.secret_token = '17dfd2a729f0274b32d9baef1141f5bfc0bd3643c8047d964d6a45c2854b50b3346906c09c3f47482c145e438d8af81f76961e896269f67d4318f15672a7deaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Xdc::Application.config.session_store :cookie_store, key: '_xdc_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Xdc::Application.config.session_store :active_record_store
